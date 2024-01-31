1189030a830351a2241c4b7c6c2829dcdbb4d88d
pankajsh5 <psharma05072000@gmail.com>
2024-01-31 12:02:25 +0530

    update

--- services/common.js
+++ services/common.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 
-exports.isAuth = (req, res, done) => {
-  return passport.authenticate("jwt");
+exports.isAuth = (req, res, next) => {
+  return passport.authenticate("jwt", { session: false })(req, res, next);
 };
 
 exports.sanitizeUser = (user) => {
@@ -15,6 +15,8 @@ exports.cookieExtractor = function (req) {
   }
   // //TODO : this is temporary token for testing without cookie
   // token =
+ 
   //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjU3ZDNiYTc1YzZmODA0MWIwNzdiZSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg5NjE1NzkxfQ.75dgR47bmiQ7hURebB44P064NPa3RpQyIlk0csRylrM";
+  console.log("cookie",token)
   return token;
 };
