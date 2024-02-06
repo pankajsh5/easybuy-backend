27678954069fe8db43d98d3d69d9c28c74cd904b
pankajsh5 <psharma05072000@gmail.com>
2024-02-06 14:49:16 +0530

    url change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended : false}));
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://easybuy-backend.vercel.app"],
+        origin: ["http://localhost:3000", "https://pankaj-e-commerce.onrender.com"],
         credentials: true,
         optionsSuccessStatus: 204,
     })
